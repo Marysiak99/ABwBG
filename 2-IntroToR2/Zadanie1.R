14cd98b56d290e914ecf2ccd29f3be418bf6b37e
Marysiak99 <marysia.b.kuczynska@gmail.com>
2024-10-15 11:01:12 +0200

    Add files via upload

---
+++ 2-IntroToR2/Zadanie1.R
@@ -0,0 +1,31 @@
+## Zadanie 1: Zapoznanie się z RStudio
+
+**Cel:** Poznanie interfejsu i podstawowych funkcji RStudio.
+
+### Instrukcje i rozwiązanie:
+
+1.  **Uruchom RStudio** na swoim komputerze.
+
+2.  **Zapoznaj się z interfejsem użytkownika:**
+  
+3.  **Stwórz nowy skrypt R:**
+  
+4.  **W skrypcie napisz komentarze** opisujące funkcje poszczególnych paneli.
+
+#------------FUNKCJE POSZCZEGÓLNYCH PANELI------
+
+#Source (edytor skryptów) słyży do pisania skryptów, a funkcje nie są automatycznie wykonywane. 
+#Console (konsola) służy do wprowadzania funkcji
+#Enviroment 
+#panel wyjściowy - ułatwienia dla Rstudio. tworzenie plików, importowanie 
+
+#----------MOTYW KOLORYSTYCZNY-------
+
+5.  **Znajdź w RStudio ustawienia** i **zmień motyw kolorystyczny** na taki, który najbardziej Ci odpowiada.
+
+6.  **Napisz w komentarzu**, jaki motyw wybrałeś/aś i dlaczego.
+#wybrałam motyw ciemny, aby zwiększyć komfort pracy 
+
+7.  **Zapisz skrypt** jako `zadanie1.R` w swoim folderze.
+
+8.  **Prześlij plik do swojego repozytorium na GitHub**.
